90084d71813cbc05ace29375aa720ecdc167b6bd
Addison Benzshawel <jbenzshawel@gmail.com>
2014-05-27 22:17:45 -0500

    fixed spacing on index/info

--- src/index.php
+++ src/index.php
@@ -1,45 +1,45 @@
 <?php require_once('header.php'); ?>
-		<!--Main Content-->
-		<ul class="images">
-		<?php
-			$i = 0;
-			foreach($images as $image){
-				if(strpos($image['link'], "/a/")){
-					$j = 0;
-					$gallery = Gallery::get_imgur_gallery($image['link']);
-					$gallery_title = $gallery['title'][0];
-					echo "<li><ul class='imgur-gal'>";
-					foreach($gallery['gallery'] as $imgur){
-						echo '<li><a class="fancybox-imgur" rel="imgur_gal' . $i . '" href="' . $imgur . '">[Gallery] ' . $gallery_title.'</a></li>' . "\n"; 
-						$j++;
-						if($j > count($gallery['gallery'])/2 - 1){
-							break;
-						}
-					}
-					echo "</ul></li>";
-					$i++;
-				} else {
-					if(substr($image['link'], -4, 1) == "."){
-						echo '<li><a class="fancybox" rel="group" title="' . $image['title'] . '" href="' . $image['link'] . '"><img class="thumbnail" src="' . $image['link'] .'" alt="'. $image['title'] . '"/></a></li>' . "\n"; 
-					} elseif (substr($image['link'], 0, 21) == 'http://www.reddit.com' ) {
-						echo '<li><a class="fancybox" rel="group" title="' . $image['title'] . '" href="'. $image['link'] .'"><img class="thumbnail" src="img/self-post.png" style="width:180px; height:auto;" alt="selfpost" /></a></li>' . "\n"; 
-					} elseif(strpos($image['link'], 'imgur') and substr($image['link'], -4, 1) != ".")  {
-						$cleaned_link = strpos($image['link'], "&") ? substr($image['link'], 0, stripos($image['link'], "&") ) : $image['link'];
-						echo "<li><a class='fancybox' rel='group'  title='{$image['title']}'  href='$cleaned_link'><img class='thumbnail' src='$cleaned_link.jpg' alt=''/></a></li>\n"; 
-					} else{
-						echo '<li><a class="fancybox" rel="group" title="' . $image['title'] . '" href="'. $image['link'] .'"><img class="thumbnail" src="img/self-post.png" style="width:180px; height:auto;" alt="selfpost" /></a></li>' . "\n"; 
-					}
+<!--Main Content-->
+<ul class="images">
+<?php
+	$i = 0;
+	foreach($images as $image){
+		if(strpos($image['link'], "/a/")){
+			$j = 0;
+			$gallery = Gallery::get_imgur_gallery($image['link']);
+			$gallery_title = $gallery['title'][0];
+			echo "<li><ul class='imgur-gal'>";
+			foreach($gallery['gallery'] as $imgur){
+				echo '<li><a class="fancybox-imgur" rel="imgur_gal' . $i . '" href="' . $imgur . '">[Gallery] ' . $gallery_title.'</a></li>' . "\n"; 
+				$j++;
+				if($j > count($gallery['gallery'])/2 - 1){
+					break;
 				}
-			} ?>
-		</ul><!--Close image list-->
+			}
+			echo "</ul></li>";
+			$i++;
+		} else {
+			if(substr($image['link'], -4, 1) == "."){
+				echo '<li><a class="fancybox" rel="group" title="' . $image['title'] . '" href="' . $image['link'] . '"><img class="thumbnail" src="' . $image['link'] .'" alt="'. $image['title'] . '"/></a></li>' . "\n"; 
+			} elseif (substr($image['link'], 0, 21) == 'http://www.reddit.com' ) {
+				echo '<li><a class="fancybox" rel="group" title="' . $image['title'] . '" href="'. $image['link'] .'"><img class="thumbnail" src="img/self-post.png" style="width:180px; height:auto;" alt="selfpost" /></a></li>' . "\n"; 
+			} elseif(strpos($image['link'], 'imgur') and substr($image['link'], -4, 1) != ".")  {
+				$cleaned_link = strpos($image['link'], "&") ? substr($image['link'], 0, stripos($image['link'], "&") ) : $image['link'];
+				echo "<li><a class='fancybox' rel='group'  title='{$image['title']}'  href='$cleaned_link'><img class='thumbnail' src='$cleaned_link.jpg' alt=''/></a></li>\n"; 
+			} else{
+				echo '<li><a class="fancybox" rel="group" title="' . $image['title'] . '" href="'. $image['link'] .'"><img class="thumbnail" src="img/self-post.png" style="width:180px; height:auto;" alt="selfpost" /></a></li>' . "\n"; 
+			}
+		}
+	} ?>
+</ul><!--Close image list-->
 <?php if($subreddit == "gonewild"): ?>
-		<a href="<?php echo '?page=' . $next_page; ?>" id="morePosts">Page <?php echo $next_page; ?></a>
+	<a href="<?php echo '?page=' . $next_page; ?>" id="morePosts">Page <?php echo $next_page; ?></a>
 <?php elseif($page == 5): ?>
 	<p>Sorry this site only goes back five pages right now. Check back for improvements!</p>
 <?php else: ?>
-		<a href="<?php echo '?subreddit=' . $subreddit . '&page=' . $next_page; ?>" id="morePosts">Page <?php echo $next_page; ?></a>
+	<a href="<?php echo '?subreddit=' . $subreddit . '&page=' . $next_page; ?>" id="morePosts">Page <?php echo $next_page; ?></a>
 <?php endif; ?>
-		<a class="exit-off-canvas"></a><!-- close the off-canvas menu -->
+	<a class="exit-off-canvas"></a><!-- close the off-canvas menu -->
 	</div> <!--End Inner Wrapper-->
 </div><!--End Off Canvas Wrapper-->
 <?php include('footer.php'); ?>
--- src/info.php
+++ src/info.php
@@ -1,30 +1,16 @@
 <?php require_once('header.php'); ?>
-<!--Off Canvas Wrapper-->
-<div class="off-canvas-wrap" data-offcanvas>
-	<div class="inner-wrap">
-		<!--Header-->
-		<header class="main">
-			<h2><a href="info.php">/r/<?php echo $subreddit; ?></a></h2>
-			<a class="right-off-canvas-toggle" href="#" >Menu</a>
-			<!-- Off Canvas Menu -->
-		    <aside class="right-off-canvas-menu">
-		        <ul class="off-canvas-list">
-		          <?php include 'sidebar.php'; ?>
-		        </ul>
-		    </aside>
-		</header><!--End Header-->
-		<!--Main Content-->
-		<div class="info-wrap">
-			<?php echo $details; ?>
-		</div>
+<!--Main Content-->
+<div class="info-wrap">
+	<?php echo $details; ?>
+</div>
 <?php if($subreddit == "gonewild"): ?>
-		<a href="<?php echo '?page=' . $next_page; ?>" id="morePosts">Page <?php echo $next_page; ?></a>
+	<a href="<?php echo '?page=' . $next_page; ?>" id="morePosts">Page <?php echo $next_page; ?></a>
 <?php elseif($page == 5): ?>
 	<p>Sorry this site only goes back five pages right now. Check back for improvements!</p>
 <?php else: ?>
-		<a href="<?php echo 'info.php?subreddit=' . $subreddit . '&page=' . $next_page; ?>" id="morePosts">Page <?php echo $next_page; ?></a>
+	<a href="<?php echo 'info.php?subreddit=' . $subreddit . '&page=' . $next_page; ?>" id="morePosts">Page <?php echo $next_page; ?></a>
 <?php endif; ?>
-		<a class="exit-off-canvas"></a><!-- close the off-canvas menu -->
+	<a class="exit-off-canvas"></a><!-- close the off-canvas menu -->
 	</div> <!--End Inner Wrapper-->
 </div><!--End Off Canvas Wrapper-->
 <?php include('footer.php'); ?>
