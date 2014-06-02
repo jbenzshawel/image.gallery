0c9f92c20d89a9759dc333e008d4a6f9df52d724
Addison Benzshawel <john-benzshawel@uiowa.edu>
2014-06-02 01:56:44 -0500

    fixed checkbox on page refresh

--- src/sidebar.php
+++ src/sidebar.php
@@ -1,11 +1,15 @@
-<?php if(!isset($link_info)){ $link_info=""; } ?>
+<?php 
+if(!isset($link_info)){ 
+	$link_info=""; 
+} 
+$checked_gifs = (isset($gifs)) ? "checked" : ""; ?>
 <li><input type="search" placeholder="search subreddits" name="s"></input><input type="submit" id="searchSub" ></input></li>
 <li><a href="info.php<?php echo $link_info;?>">Details View</a></li>
 <li><a href="index.php<?php echo $link_info;?>">Gallery View</a></li>
 <li>
 	<form action="" id="sidebar_form" method="POST">
 		<label for="load_gifs">Load gifs?</label>
-		<input type="checkbox" id="load_gifs" name="gifs" value="checked"/>
+		<input type="checkbox" id="load_gifs" name="gifs" value="checked" <?php echo $checked_gifs; ?>/>
 		<input type="image" value="submit" src="img/refresh.png"/>
 	</form>
 </li>
